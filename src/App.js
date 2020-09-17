65d22db9fe7ff8e58004c08a20e93f7648171e54
Greg Schuman <gcschuman@hotmail.com>
2020-09-17 10:31:18 -0700

    Added props to Person.js.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component{
       <div className="App">
         <h1>This is a React app</h1>
         <p>It's working! Mwahahahaha!</p>
-        <Person />
+        <Person name="Alan" age="20"/>
+        <Person name="Mohamed" age="23"/>
+        <Person name="Jessica" age="34"/>
       </div>
     )
   }
