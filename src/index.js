148e0b5132a8814010fca18247801797944057a2
Greg Schuman <gcschuman@hotmail.com>
2020-09-17 10:15:29 -0700

    Added Person folder and Person.js.

--- src/index.js
+++ src/index.js
@@ -11,22 +11,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-class Board extends React.Component{
-  renderSquare(i){
-    return <Square value={i} />;
-  }
-}
-
-class Square extends React.Component{
-  render(){
-    return(
-      <button className="square">
-        {this.props.value}
-      </button>
-    )
-  }
-}
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
