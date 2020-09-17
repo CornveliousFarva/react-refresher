9d5b193a7c877f82c3a74b6c42a17e7f6635afcd
Greg Schuman <gcschuman@hotmail.com>
2020-09-17 11:11:37 -0700

    Added a handler for the switch button.

--- src/App.js
+++ src/App.js
@@ -10,13 +10,16 @@ class App extends Component{
       { name: 'Jessica', age: 34}
     ]
   }
-
+  switchNameHandler = () => {
+    console.log('clicked!')
+  }
+  
   render(){
     return (
       <div className="App">
         <h1>This is a React app</h1>
         <p>It's working! Mwahahahaha!</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: Soccer </Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
